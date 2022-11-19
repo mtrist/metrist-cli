6e365f3ae390126f0919e10b220df5ebabdfe5b8
parsasi <mail@parsa.pro>
2022-11-19 09:18:08 -0800

    Experimenting with sandboxing

--- actions/sync.action.ts
+++ actions/sync.action.ts
@@ -1,17 +1,50 @@
 import * as chalk from 'chalk';
-import { readFileSync } from 'fs';
+import { readFileSync, promises } from 'fs';
 import { join } from 'path';
 import { AbstractAction } from './abstract.action';
 import { MESSAGES } from '../lib/ui';
 import { RcFile } from '../interfaces/rc.interface';
 import { readEnv } from '../lib/env/readEnv';
+import { exec } from 'js-exec';
+import { transform } from '@babel/core';
+import env from 'babel-preset-env';
 
 export class SyncAction extends AbstractAction {
   public async handle() {
-    console.log(await readEnv());
     try {
-      const rcPath = this.getRCFile();
+      const rcFile = this.getRCFile();
+      if (rcFile.fetcher) {
+        const content = await promises.readFile(
+          join(process.cwd(), rcFile.fetcher),
+          'utf8',
+        );
+        let value: any;
+        const setValue = (val: any) => (value = val);
+
+        transform(
+          content,
+          {
+            presets: [env],
+          },
+          (err: Error, results: { code: string }) => {
+            // console.log(results.code);
+            exec(results.code, {
+              onSuccess: () => {
+                console.log('the code said: ', value);
+              },
+              onError: (error) => {
+                console.log('error');
+                console.error(error);
+              },
+            })({
+              console,
+              setContent: setValue,
+            });
+          },
+        );
+      }
     } catch (e) {
+      console.log(e);
       console.error(chalk.red(MESSAGES.RC_FILE_NOT_FOUND));
     }
   }
--- interfaces/rc.interface.ts
+++ interfaces/rc.interface.ts
@@ -1,3 +1,4 @@
 export interface RcFile {
   resolvePath?: string;
+  fetcher?: string;
 }

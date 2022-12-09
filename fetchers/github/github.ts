5c341e7771a414daf1d60897fd61e625a3b649ed
parsasi <mail@parsa.pro>
2022-12-09 10:08:16 -0800

    WIP: working on gihtub

--- fetchers/github/github.ts
+++ fetchers/github/github.ts
@@ -1,25 +1,33 @@
 import axios from 'axios';
+import { languages } from '../../constants/languages.constant';
+
+const getContentsGetter = (user , repo)
 
 export const getRepositoryData = async (url: string) => {
   const [, user, repo] =
     url.match(/https:\/\/github.com\/([^/]+)\/([^/]+)/) || [];
 
   // make an API call to GitHub to get the repository data
-  const { data } = await axios.get(
-    `https://api.github.com/repos/${user}/${repo}`,
+  const repository = await axios.get(
+    `https://api.github.com/repos/${user}/${repo}/contents`,
   );
+
+  const directories = repository.data.filter((item) => item.type === 'dir');
 
   // create an object to store the data for each language
   const languageData = {};
 
   // iterate over the repository's directories and get the language code and files in each language directory
-  for (const directory of data.directories) {
-    const [, languageCode] = directory.name.match(/^([^/]+)\//) || [];
+  for (const directory of directories) {
+    const languageCode = directory.name;
 
-    if (!languageCode) continue;
+    const directoryFiles = await 
+
+    if (!languages[languageCode]) continue;
 
     // create an inner object for each language to store the filenames and parsed JSON values
     languageData[languageCode] = {};
+    
 
     // iterate over the files in the language directory and parse the JSON data
     for (const file of directory.files) {
